1bb9c05a6b523fc8a1c903801db69d0762742b9d
Heather Forester <hforeste07@gmail.com>
2025-09-18 14:34:38 +0000

    wired up right

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,14 @@ class BoardsController < ApplicationController
   def index
     render({ :template => "board_templates/index" })
   end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_boards = Board.where({ :id => the_id })
+
+    @the_board = matching_boards.at(0)
+
+    render({ :template => "board_templates/show" })
+  end
 end
