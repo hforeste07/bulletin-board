6c0d16a703846851e4d386b609981f76770d1610
Heather Forester <hforeste07@gmail.com>
2025-09-18 17:14:54 +0000

    error with .errors on @new_board.id

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,5 +8,6 @@
 #  updated_at :datetime         not null
 #
 class Board < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, :presence => true
+  validates :name, :uniqueness => true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "boards", :action => "index" })
+  get("/boards", { :controller => "boards", :action => "index" })
 
   get("/boards/:path_id", { :controller => "boards", :action => "show" })
 
