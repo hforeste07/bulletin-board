a84a8e706c4cbfc18989abd7122cca547b478134
Heather Forester <hforeste07@gmail.com>
2025-09-18 16:10:53 +0000

    notices

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,16 @@ class BoardsController < ApplicationController
   def show
     render({ :template => "board_templates/show" })
   end
+
+  def create
+    # params: {"name_param" => "Merch Mart"}
+
+    @new_board = Board.new
+    @new_board.name = params.fetch("name_param")
+    @new_board.save
+
+    #render({ :template => "board_templates/create_confirm" })
+
+    redirect_to("/boards/#{@new_board.id}", :notice => "Board created successfully.")
+  end
 end
